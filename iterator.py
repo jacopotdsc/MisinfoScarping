78076b8a61875d2c2d4668231c02506444507331
Jacopo Tedeschi <77614941+jacopotdsc@users.noreply.github.com>
2022-10-18 22:35:22 +0200

    final version of iterator
    correction of bug: now iteratore in all file. previous version select a wrong path. This version html_check is better because now it select correctly a json_file

--- iterator.py
+++ iterator.py
@@ -1,36 +1,47 @@
+import json
 from msilib.schema import Directory
 import os
 from pickle import GLOBAL
 from tkinter.tix import DirList
+from tracemalloc import start
+from unittest import result
 import html_check as hc
 from time import sleep
 import pandas as pd
+import time
 
 
 #### GLOBAL VARIABLE
-GLOBAL_DIRECTORY1 = "C:\\Users\\pc\\Desktop\\dataset thesys project" 
+GLOBAL_DIRECTORY1 = "C:\\Users\\pc\\Desktop\\dataset thesys project\\try_folder_scan" 
 GLOBAL_DIRECTORY2 = "C:\\My Web Sites\\factanews\\facta.news" 
 GLOBAL_DIRECTORY3 = "C:\\My Web Sites\\factanews"
 GLOBAL_DIRECTORY4 = "C:\\My Web Sites"
-GLOBAL_DIRECTORY5 = "C:\\misinfo"    # biggest folder: error
+GLOBAL_DIRECTORY5 = "C:\\misinfo"   
 
 # set this variabile to choose to path to analize
-USE_DIRECTORY = GLOBAL_DIRECTORY2
+USE_DIRECTORY = GLOBAL_DIRECTORY4
 
 #### VARIABLE STATUS SCANN ####
-PRINT_SCAN_STATUS = False   # to print status of iteration
-VAR_SLEEP = 0               # variabile to read print of scan
+PRINT_SCAN_STATUS = True   # to print status of iteration
+VAR_SLEEP = 0.0            # variabile to read print of scan
 AUXLIARY_FOR_FORMAT = 0     # variable for print
+FREQ = 500                  # every how much print status
 HTML_EXAMINATED = 0         # counter for html examinated
 FOLDER_SCANNED = 0          # counter for folder scanned
 TOTAL_FILE_SCANNED = 0      # counter for total scan / iteration
+TOTAL_CLAIM_REVIEW_FILE = 0 # number of claim reviewed file
+
+def increment_claim_reviewed_json():
+    global TOTAL_CLAIM_REVIEW_FILE
+
+    TOTAL_CLAIM_REVIEW_FILE = TOTAL_CLAIM_REVIEW_FILE + 1
 
 def increment_total_file_scanned():
     global TOTAL_FILE_SCANNED
     
     TOTAL_FILE_SCANNED = TOTAL_FILE_SCANNED + 1
 
-    if AUXLIARY_FOR_FORMAT % 1000 == 0 and AUXLIARY_FOR_FORMAT > 500:
+    if AUXLIARY_FOR_FORMAT % FREQ == 0 and AUXLIARY_FOR_FORMAT > 500:
         if PRINT_SCAN_STATUS == True:
             print("-- total file scanned until now: " + str(TOTAL_FILE_SCANNED))
             sleep(VAR_SLEEP)
@@ -42,7 +53,7 @@ def increment_html_examinated():
     HTML_EXAMINATED = HTML_EXAMINATED + 1
     AUXLIARY_FOR_FORMAT = AUXLIARY_FOR_FORMAT + 1
 
-    if AUXLIARY_FOR_FORMAT % 1000 == 0:
+    if AUXLIARY_FOR_FORMAT % FREQ == 0:
         if PRINT_SCAN_STATUS == True:
             print("-- html file examinated until now: " + str(HTML_EXAMINATED)) 
             sleep(VAR_SLEEP)
@@ -52,12 +63,53 @@ def increment_folder_scannned():
 
     FOLDER_SCANNED = FOLDER_SCANNED + 1
 
-    if AUXLIARY_FOR_FORMAT % 1000 == 0 and AUXLIARY_FOR_FORMAT > 500:
+    if AUXLIARY_FOR_FORMAT % FREQ == 0 and AUXLIARY_FOR_FORMAT > 500:
         if PRINT_SCAN_STATUS == True:
             print("-- total folder scanned until now: " + str(FOLDER_SCANNED))
             sleep(VAR_SLEEP)
 
 html_array = []
+
+def make_flat_dict(json_file, new_dict, depth=0):
+
+    #print(type(json_file))
+    #print(json_file)
+
+    #print()
+    #print("key_list: " + str(json_file.keys()) )
+    for key in json_file.keys():        # scan every key
+        
+        value = json_file[key]
+        #print("key: " + str(key) + " -> " + str(value))
+        
+
+        if str(  type(value)  ) == "<class 'dict'>":
+            #print("-- dict:  recursion on dict, " + str(type(value)))
+            make_flat_dict(value, new_dict,depth+1)
+
+        elif str(  type(value)  ) == "<class 'list'>":    # if it's a list, # check if list of dict
+
+            #print("--- list: recursion on list, " + str(type(value)))
+            for elem in value:
+                if str(type(elem)) == "<class 'str'>":
+                    new_key = key + "." + str(depth)
+                    new_dict[new_key] = []
+                    new_dict[new_key].append(elem)
+                else:
+                    make_flat_dict(elem,new_dict, depth + 1)
+        else:
+            #print("--- str: append,  " + str(type(value)))
+            new_key = str(key) + "." + str(depth)
+
+            if new_key in new_dict.keys():
+                new_dict[new_key].append(value)
+            else:
+                new_dict[new_key] = []
+                new_dict[new_key].append(value)
+
+    return new_dict
+
+
 
 def iterate_on_folders(directory):
 
@@ -68,14 +120,24 @@ def iterate_on_folders(directory):
 
         # analize html
         if filename.endswith(".html"):  
-            result = hc.get_json([filename])
+            new_path = directory + "\\" + filename
+            result = hc.get_claimReviewed_scheme_in_html_code(new_path)
+            
+            # result is an array of < [ json_file, ... ], lang >     
+            for file_json in result[0]:
 
-            # result is an array of < json_file, lang >
-            for r in result:
-                for file_json in r[0]:
-                    new_json = file_json
-                    new_json['lang'] = r[1]
-                    html_array.append(new_json)
+                if file_json == []:
+                    break
+                else:
+                    increment_claim_reviewed_json()
+
+                new_json = file_json
+                new_json['lang'] = result[1]
+
+                new_dict = {} 
+                flattend_dict = make_flat_dict(new_json, new_dict)
+            
+                html_array.append(flattend_dict)
 
             increment_html_examinated()
 
@@ -90,38 +152,51 @@ def iterate_on_folders(directory):
                 continue
     
 
-def create_csv(data_array):
-    print("--- creating csv ---")
-
-    if len(data_array) == 0:
-        print("--- NO DATA COLLECTED ---")
-        return
-
-
-    my_file = open("data.csv","w", encoding="utf-8")
-
-    #   I prepare file as a csv
-    my_file.write("json,lang\n")
-
-
-    for data in data_array:
-        my_file.write(str(data) + "," + data['lang'] + "\n")
-
-    my_file.close()
-
-
+def create_csv(dataframe):
+    dataframe.to_csv("data.csv")
+    
 def create_dataset(data_array):
     return pd.DataFrame(data_array)
 
 def main():
+
+    start_time = time.time()
     iterate_on_folders(USE_DIRECTORY)
-    create_csv(html_array)
+    end_time = time.time() 
+    total_time = end_time - start_time
+    #print("total_time: %s" + str(total_time))
+    print(total_time)
+    
+    '''
+    result = hc.get_claimReviewed_scheme_in_html_code("try_folder_scan\\mucche_latte.html")
+
+    print(result[0][0])
+    print(len(result[0]))
+    for file_json in result[0]:
+        new_json = file_json
+        new_json['lang'] = result[1]
+
+        new_dict = {} 
+        flatten_dict = make_flat_dict(new_json, new_dict)
+
+        
+        print("flatten dict")
+
+
+        for k in flatten_dict.keys():
+            print("-- " + str(k) +  " -> " + str(flatten_dict[k]) )
+        print()
+    '''
+        
+
     print("\ntotal html file examinated: " + str(HTML_EXAMINATED))
     print("total folder scanned: " + str(FOLDER_SCANNED))
     print("total file scanned: " + str(TOTAL_FILE_SCANNED))
+    print("total json claim reviewed found: " + str(TOTAL_CLAIM_REVIEW_FILE))
 
     dataset = create_dataset(html_array)
-    print(dataset)
+    #print(dataset.head())
+    create_csv(dataset)
 
 
 main()
--- nlp-module.py
+++ nlp-module.py
@@ -20,10 +20,39 @@ import time
 START_MAIN_TEXT = "<div class=\"edgtf-post-text-main\">"
 END_MAIN_TEXT   = "<div class=\"edgtf-post-info-bottom clearfix\">"
 HTML_TEXT_TAG   = "<p>"
-HTML_WORDS      = ["<p>", "<a href", ">", "<", "/", "href", "https", "="]
+HTML_WORDS      = ["<a", "<p>", "<a href", ">", "<", "/", "href", "https", "=", "target=", "rel=" ]
 
 MY_STOPSORDS_EN = STOPWORDS
-MY_STOPSORDS_IT = ['avere', 'stato', 'sei', 'non potevo', 'di', 'Appena', 'fuori', 'tutto', 'il', 'voluto', 'non', 'non deve', 'sono', 'ottenere', 'io sono', 'questo', 'io', 'non lo farebbe', 'non lo sono', 'Fino a', 'fatto', 'no', 'però', 'una volta', 'il vostro', 'Sopra', 'capannone', 'andiamo', 'anche', 'lui stesso', 'tale', 'lo farai', 'me', 'id', 'loro stessi', 'non lo erano', 'pochi', 'erano', "cos'è", 'la sua', 'quindi', 'sopra', 'o', 'mio', 'fra', 'dopo', 'quando è', 'me stesso', 'in occasione', 'insieme a', 'quale', 'ecco', 'K', 'spento', 'suo', 'loro', 'com', 'è', 'ulteriore', 'lo faresti', 'non lo fa', 'se stessa', 'in', 'non posso', 'là', 'a', 'come', 'Su', 'ha', 'Se', 'lei', 'stesso', 'lo faremmo', 'non', 'fuori uso', 'non farlo', 'solo', 'noi stessi', 'e', 'mentre', 'avendo', 'ma', 'lui', 'quello è', 'perché', 'Quello', 'il suo', 'perché', 'in', 'essi', 'lei è', 'malato', 'noi', 'chi', 'non ho', 'per', 'altro', 'Entrambi', 'dove', 'non dovrebbe', 'essere', 'facendo', 'mai', 'queste', 'poi', 'noi abbiamo', 'quando', 'r', 'non ha', 'altrimenti', 'a testa', 'sotto', 'www', 'lo faranno', 'lui è', 'non lo è', 'hanno', 'un', "dov 'è", 'di', "c'è", 'http', 'si', 'quelli', 'anche', 'sotto', 'contro', 'come', 'Così', 'molto', 'che cosa', 'io ho', 'te stesso', 'lui', 'chi è', 'qui', 'i nostri', 'qualunque', 'Potere', 'più', 'non lo era', 'a', 'non', 'sono', 'bene', 'essendo', 'piace', 'hai', 'tuo', 'fa', 'non aveva', 'Potevo', 'di', "com'è", 'esso', 'su', 'fare', 'era', 'Loro sono', 'ancora', 'prima', 'non può', 'lo avrebbero fatto', 'un', 'voi stessi', 'da', 'da', 'inferno', 'nostro', 'Di più', 'deve', 'dunque', 'dovrebbe', 'di', 'il loro', 'guscio', 'Altro', 'No', 'perché', 'dovrebbe', 'possedere', 'né', 'voi', 'è', 'erano', 'chi', 'lui', 'suo', 'alcuni', 'attraverso', 'i loro', 'avevo']
+#MY_STOPSORDS_IT = ['avere', 'stato', 'sei', 'non potevo', 'di', 'Appena', 'fuori', 'tutto', 'il', 'voluto', 'non', 'non deve', 'sono', 'ottenere', 'io sono', 'questo', 'io', 'non lo farebbe', 'non lo sono', 'Fino a', 'fatto', 'no', 'però', 'una volta', 'il vostro', 'Sopra', 'capannone', 'andiamo', 'anche', 'lui stesso', 'tale', 'lo farai', 'me', 'id', 'loro stessi', 'non lo erano', 'pochi', 'erano', "cos'è", 'la sua', 'quindi', 'sopra', 'o', 'mio', 'fra', 'dopo', 'quando è', 'me stesso', 'in occasione', 'insieme a', 'quale', 'ecco', 'K', 'spento', 'suo', 'loro', 'com', 'è', 'ulteriore', 'lo faresti', 'non lo fa', 'se stessa', 'in', 'non posso', 'là', 'a', 'come', 'Su', 'ha', 'Se', 'lei', 'stesso', 'lo faremmo', 'non', 'fuori uso', 'non farlo', 'solo', 'noi stessi', 'e', 'mentre', 'avendo', 'ma', 'lui', 'quello è', 'perché', 'Quello', 'il suo', 'perché', 'in', 'essi', 'lei è', 'malato', 'noi', 'chi', 'non ho', 'per', 'altro', 'Entrambi', 'dove', 'non dovrebbe', 'essere', 'facendo', 'mai', 'queste', 'poi', 'noi abbiamo', 'quando', 'r', 'non ha', 'altrimenti', 'a testa', 'sotto', 'www', 'lo faranno', 'lui è', 'non lo è', 'hanno', 'un', "dov 'è", 'di', "c'è", 'http', 'si', 'quelli', 'anche', 'sotto', 'contro', 'come', 'Così', 'molto', 'che cosa', 'io ho', 'te stesso', 'lui', 'chi è', 'qui', 'i nostri', 'qualunque', 'Potere', 'più', 'non lo era', 'a', 'non', 'sono', 'bene', 'essendo', 'piace', 'hai', 'tuo', 'fa', 'non aveva', 'Potevo', 'di', "com'è", 'esso', 'su', 'fare', 'era', 'Loro sono', 'ancora', 'prima', 'non può', 'lo avrebbero fatto', 'un', 'voi stessi', 'da', 'da', 'inferno', 'nostro', 'Di più', 'deve', 'dunque', 'dovrebbe', 'di', 'il loro', 'guscio', 'Altro', 'No', 'perché', 'dovrebbe', 'possedere', 'né', 'voi', 'è', 'erano', 'chi', 'lui', 'suo', 'alcuni', 'attraverso', 'i loro', 'avevo']
+MY_STOPSORDS_IT = ['nel','del', 'le', 'con', 'dalla', 'il', 'dal', 'al', 'la', 
+                    'che', 'avere', 'stato', 'sei', 'non potevo', 'di', 'Appena', 
+                    'fuori', 'tutto', 'il', 'voluto', 'non', 'non deve', 'sono', 
+                    'ottenere', 'io sono', 'questo', 'io', 'non lo farebbe', 'non lo sono', 
+                    'Fino a', 'fatto', 'no', 'però', 'una volta', 'il vostro', 'Sopra', 
+                    'capannone', 'andiamo', 'anche', 'lui stesso', 'tale', 'lo farai', 
+                    'me', 'id', 'loro stessi', 'non lo erano', 'pochi', 'erano', "cos'è", 
+                    'la sua', 'quindi', 'sopra', 'o', 'mio', 'fra', 'dopo', 'quando è', 
+                    'me stesso', 'in occasione', 'insieme a', 'quale', 'ecco', 'K', 
+                    'spento', 'suo', 'loro', 'com', 'è', 'ulteriore', 'lo faresti', 'non lo fa', 
+                    'se stessa', 'in', 'non posso', 'là', 'a', 'come', 'Su', 'ha', 'Se', 
+                    'lei', 'stesso', 'lo faremmo', 'non', 'fuori uso', 'non farlo', 
+                    'solo', 'noi stessi', 'e', 'mentre', 'avendo', 'ma', 'lui', 'quello è', 
+                    'perché', 'Quello', 'il suo', 'perché', 'in', 'essi', 'lei è', 'malato', 
+                    'noi', 'chi', 'non ho', 'per', 'altro', 'Entrambi', 'dove', 'non dovrebbe', 
+                    'essere', 'facendo', 'mai', 'queste', 'poi', 'noi abbiamo', 'quando', 'r', 
+                    'non ha', 'altrimenti', 'a testa', 'sotto', 'www', 'lo faranno', 'lui è', 'non lo è',
+                     'hanno', 'un', "dov 'è", 'di', "c'è", 'http', 'si', 'quelli', 'anche', 'sotto', 
+                     'contro', 'come', 'Così', 'molto', 'che cosa', 'io ho', 'te stesso', 'lui', 'chi è', 
+                     'qui', 'i nostri', 'qualunque', 'Potere', 'più', 'non lo era', 'a', 'non', 'sono', 
+                     'bene', 'essendo', 'piace', 'hai', 'tuo', 'fa', 'non aveva', 'Potevo', 'di', "com'è", 
+                     'esso', 'su', 'fare', 'era', 'Loro sono', 'ancora', 'prima', 'non può', 'lo avrebbero fatto', 
+                     'un', 'voi stessi', 'da', 'da', 'inferno', 'nostro', 'Di più', 'deve', 'dunque', 'dovrebbe', 
+                     'di', 'il loro', 'guscio', 'Altro', 'No', 'perché', 'dovrebbe', 'possedere', 'né', 'voi', 'è',
+                      'erano', 'chi', 'lui', 'suo', 'alcuni', 'attraverso', 'i loro', 'avevo'
+                      
+                    ]
+
+
 
 my_stowords_dict = {'it': MY_STOPSORDS_IT, 'en': MY_STOPSORDS_EN}
 
@@ -31,7 +60,8 @@ my_stowords_dict = {'it': MY_STOPSORDS_IT, 'en': MY_STOPSORDS_EN}
 def check_if_contain_html_words_or_stopwords(word, stopwords):
     
     for sw in stopwords:
-        if word.find(sw) != -1:
+        ##if word.find(sw) != -1:
+        if word == sw:
             return True
     
     return False
@@ -88,8 +118,6 @@ def translate_html_text(text):
 
     splitted_text = text.split("<p>")   # I need to split because is too long for translator
     final_translated_text = ''
-    
-    #print("entro, len: " + str(len(splitted_text)) )
 
     for i in range( len(splitted_text) ):
 
@@ -126,21 +154,78 @@ def translate_stopwords(array, s='auto', t='it'):
         translated_array.append(p)
     
     return translated_array
-    
+
+
+def clear_from_url(text):
+    new_line = ''
+
+    i = 0
+    while i < len(text):
+        if text[i] == '<' and text[i+1] == 'a' and text[i+3] == 'h' and text[i+4] == 'r' and text[i+5] == 'e' and text[i+6] =='f':
+            
+            while( text[i] != ">" ): 
+                i += 1
+            i += 1
+            while( text[i] != "<"):
+                new_line += text[i]
+                i += 1
+            
+            while( text[i] != ">"):
+                i += 1
+        else:
+            new_line += text[i]
+        
+        i += 1
+
+    return new_line
+            
+
+# take html_main_text and return text without html tags
+def clear_html_text(text):
+
+    new_text = ''
+    strange_a_letter = "Â"
+
+    text = [text]
+    for line in text:
+        new_line = ''
+
+        if "<a href" in line:
+            new_line = clear_from_url(line)
+            
+
+        print(new_line)
+        print("entrato p")
+        if new_line == '':
+            new_line = line
+            
+        print(new_line)
+        new_line = new_line.split("<p>")[1]
+        print(new_line)
+        new_line = new_line.split("</p>")[0]
+        print(new_line)
+
+        new_text += new_line
+
+        print("splitto")
+        if strange_a_letter in new_text:
+            #print(new_text.split(strange_a_letter))
+            for w in new_text.split(strange_a_letter):
+                new_text += w
+
+    return new_text
 
 
 def extract_keywords(html_file, max_common_words = 10):
-    html_code = open(html_file,'r',errors="ignore")
+    html_code = open(html_file,'r',errors="ignore", encoding='UTF-8')
 
 
     all_headlines = extract_main_text(html_code)
-    #print(all_headlines_it)
-    #all_headlines = translate_text(all_headlines_it[:3000])
-
-    #print(all_headlines)
 
     stopwords = create_stopwords_set()
     wordcloud = WordCloud(stopwords=stopwords, background_color="white", max_words=1000).generate(all_headlines[0:400])
+
+    print(all_headlines)
 
     rcParams['figure.figsize'] = 5, 10
     plt.imshow(wordcloud)
@@ -149,12 +234,17 @@ def extract_keywords(html_file, max_common_words = 10):
 
     # this array will contain all words on the text
     filtered_words = [word for word in all_headlines.split() if word not in stopwords and check_if_contain_html_words_or_stopwords(word, stopwords) == False ]# and word.isnumeric() == False ]
+    #filtered_words = [word for word in all_headlines.split() if word not in stopwords and check_if_contain_html_words_or_stopwords(word, stopwords) == False and word.isnumeric() == False ]
+    
+    #filtered_words = [word for word in all_headlines.split() if word not in stopwords]
+
     counted_words = collections.Counter(filtered_words)
     words = []
     counts = []
-    for letter, count in counted_words.most_common(max_common_words):
+    for letter, count in counted_words.most_common(30):
         words.append(letter)
         counts.append(count)
+        print(letter + " -> " + str(count) )
 
     colors = cm.rainbow(np.linspace(0, 1, 10))
     rcParams['figure.figsize'] = 10, 5
@@ -168,12 +258,19 @@ def extract_keywords(html_file, max_common_words = 10):
 
 
 
-start_time = time.time()
-print(translate_stopwords(MY_STOPSORDS_EN))
-print("--- %s seconds ---" % (time.time() - start_time))
-
-file1 = extract_keywords("gasdotto.html")
+#file1 = extract_keywords("gasdotto.html")
 file2 = extract_keywords("confine_russia_finlandia.html")
 
-print(file1)
+#print(file1)
 print(file2)
+
+
+'''
+text = '<p>La mattina del 21 settembre Vladimir Putin <a href="http://en.kremlin.ru/events/president/news/69390" target="_blank" rel="noreferrer noopener">ha annunciato</a> una â€œmobilitazione parzialeâ€ in Russia per reclutare nuovi soldati da mandare in guerra in Ucraina. Il presidente russo ha precisato che verranno coinvolti in primo luogo i riservisti militari, cioÃ¨ Â«coloro che hanno prestato servizio in precedenza nelle forze armateÂ».Â </p>'
+link_clear = clear_from_url(text)
+p_clear = clear_html_text(text)
+
+print("---risultati---")
+print(p_clear)
+'''
+
